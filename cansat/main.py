c05ba4809461a12ecb603d3433ec6ac73efdda27
Dagorius <dago.vp07@gmail.com>
2025-01-05 15:47:49 +0100

    Start of transmitting data (by use of time intervals)

--- cansat/main.py
+++ cansat/main.py
@@ -1,13 +1,34 @@
-from sensors import BMP280, RFM69HCW, MPU6050
+from sensors import BMP280, RFM69HCW, MPU6050 #importing sensors and radio transmitter
 import time
 
 
 bmp_sensor = BMP280()
+mpu_sensor = MPU6050()
+
 radio = RFM69HCW()
 
-while True:
-    temp = bmp_sensor.temperature
-    press = bmp_sensor.pressure
+start_time = time.time()
 
-    radio.transmit(data=f"temperature: ")
+while True:
+    current_time = time.time()
+    elapsed_time_ms = (current_time - start_time) * 1000  # Elapsed time in milliseconds
+
+
+    if 100 <= elapsed_time_ms < 150 :
+        bmp_temp = bmp_sensor.temperature
+        print(f"BMP280 Temp: {bmp_temp} C")
+
+    if 175 <= elapsed_time_ms < 250:
+        accel_data = mpu_sensor.accel
+        gyro_data = mpu_sensor.gyro
+        mpu_temp = mpu_sensor.temperature
+
+
+        message = f"Accel (x,y,z): {accel_data[0]},{accel_data[1]},{accel_data[2]} | " f"Gyro (x,y,z): {gyro_data[0]},{gyro_data[1]},{gyro_data[2]} | "             f"IMU Temp: {mpu_temp} C"
+
+        radio.transmit(message)
+        print(f"Sent: {message}")
+
+
+        start_time = time.time()
 
