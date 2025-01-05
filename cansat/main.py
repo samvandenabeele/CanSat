bed115c46713e3daac43c504a51464f844ce4a63
Sam Vandenabeele <sam.vandenabeele.09@gmail.com>
2025-01-05 13:42:26 +0100

    completed libraries

--- cansat/main.py
+++ cansat/main.py
@@ -13,6 +13,15 @@
 # pin.off()
 # print("Finished.")
 
-from sensors import BMP280
+from sensors import BMP280, RFM69HCW, MPU6050
 
-sensor = BMP280(21, 22)
+bmp280 = BMP280()
+radio = RFM69HCW()
+
+while True:
+    temp = bmp280.temperature
+    press = bmp280.pressure
+
+    radio.transmit(data=f"temperature: ")
+
+    
--- cansat/sensors/MPU6050.py
+++ cansat/sensors/MPU6050.py
@@ -9,15 +9,27 @@ imu = mpu6050(i2c)
 class MPU6050:
     def __init__(self):
         self.i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
-        imu = mpu6050(self.i2c)
+        self.imu = mpu6050(self.i2c)
 
-while True:
-    ax=round(imu.accel.x,2)
-    ay=round(imu.accel.y,2)
-    az=round(imu.accel.z,2)
-    gx=round(imu.gyro.x)
-    gy=round(imu.gyro.y)
-    gz=round(imu.gyro.z)
-    tem=round(imu.temperature,2)
-    print("ax",ax,"\t","ay",ay,"\t","az",az,"\t","gx",gx,"\t","gy",gy,"\t","gz",gz,"\t","Temperature",tem,"        ",end="\r")
-    sleep(0.2)
+    @property
+    def temperature(self):
+        return self.imu.temperature
+    
+    @property
+    def accel(self):
+        return [self.imu.accel.x, self.imu.accel.y, self.imu.accel.z]
+    
+    @property
+    def gyro(self):
+        return [self.imu.gyro.x, self.imu.gyro.y, self.imu.gyro.z]
+
+# while True:
+#    ax=round(imu.accel.x,2)
+#    ay=round(imu.accel.y,2)
+#    az=round(imu.accel.z,2)
+#    gx=round(imu.gyro.x)
+#    gy=round(imu.gyro.y)
+#    gz=round(imu.gyro.z)
+#    tem=round(imu.temperature,2)
+#    print("ax",ax,"\t","ay",ay,"\t","az",az,"\t","gx",gx,"\t","gy",gy,"\t","gz",gz,"\t","Temperature",tem,"        ",end="\r")
+#    sleep(0.2)
