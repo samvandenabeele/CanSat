0d00da2f46314de367679c1e54d94077154b8d7b
Sam Vandenabeele <sam.vandenabeele.09@gmail.com>
2025-01-05 15:29:00 +0100

    Add MPU6050 sensor integration and RFM69 receive method

--- cansat/main.py
+++ cansat/main.py
@@ -16,11 +16,16 @@
 from sensors import BMP280, RFM69HCW, MPU6050
 
 bmp280 = BMP280()
+mpu_sensor = MPU6050()
 radio = RFM69HCW()
 
 while True:
     temp = bmp280.temperature
     press = bmp280.pressure
+
+    accel_x, accel_y, accel_z = mpu_sensor.accel
+    gyro_x, gyro_y, gyro_z = mpu_sensor.gyro
+    
 
     radio.transmit(data=f"temperature: ")
 
